c9628c32a27138ddb6dc0a86921b5fef10cab8bb
Cat Yanish <catyanish@gmail.com>
2018-01-31 20:44:49 -0600

    rendering components all day long

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import ReactDOM from 'react-dom'
 
-import App from './components/app';
-import reducers from './reducers';
+//create a new component
+//should produce some html
+const App = () => {
+  return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this component's HTML and
+//put in the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
