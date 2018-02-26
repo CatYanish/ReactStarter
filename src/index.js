07b6bdadf841a87838080165ed6f5a765507d240
Cat Yanish <catyanish@gmail.com>
2018-02-25 22:03:19 -0600

    classes and states and apis

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search'
 
+const API_KEY = 'AIzaSyDadaMRvYYtnGBpf0gp_Ze0rv5IzVJVbxA'
+
+
+YTSearch({key: API_KEY, term: 'surfboads'}, function(data) {
+  console.log(data);
+})
 //create a new component
 //should produce some html
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's HTML and
